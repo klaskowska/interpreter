b66f1fb520bca09f1b7c53f821de3b1627115c1e
Karolina Laskowska <karolina.laskowska19@gmail.com>
2023-05-14 01:05:44 +0200

    Tide test script

--- test.sh
+++ test.sh
@@ -3,25 +3,49 @@
 TMP_OUT=$(mktemp)
 TMP_ERR=$(mktemp)
 
-for file in ./good/*.myl
-do
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NO_COL='\033[0m'
 
-echo "Run test:" ${file%.myl}
+passed=0
+failed=0
 
-chmod +x $file
+run_tests () {
+    dir_name=$1
+    for file in $dir_name
+    do
 
-./interpreter "$file" > $TMP_OUT $2 > $TMP_ERR
+        echo "Run test:" ${file%.myl}
 
-cat $TMP_ERR
+        chmod +x $file
 
-if diff ${file%myl}out $TMP_OUT > /dev/null;
-then echo -e "OK IN OUTPUT"
-else echo -e "ERROR IN OUTPUT"
-fi
+        ./interpreter "$file" 1>$TMP_OUT 2>$TMP_ERR
 
-if diff ${file%myl}err $TMP_ERR > /dev/null;
-then echo -e "OK IN ERROR"
-else echo -e "ERROR IN ERROR"
-fi
+        if diff ${file%myl}out $TMP_OUT > /dev/null;
+        then echo -e "OK IN STDOUT"
+            is_output_ok=true
+        else echo -e "${RED}ERROR IN STDOUT${NO_COL}"
+            is_output_ok=false
+            echo $TMP_OUT
+        fi
 
-done
+        if diff ${file%myl}err $TMP_ERR > /dev/null;
+        then echo -e "OK IN STDERR"
+            if $is_output_ok
+            then passed=$((passed + 1))
+            else failed=$((failed + 1))
+            fi
+        else echo -e "${RED}ERROR IN STDERR${NO_COL}"
+            failed=$failed + 1
+            echo $TMP_ERR
+        fi
+
+
+    done
+}
+
+run_tests "./good/*.myl"
+run_tests "./bad/*.myl"
+
+echo -e "${GREEN}Passed ${passed} tests${NO_COL}"
+echo -e "${RED}Failed ${failed} tests${NO_COL}"
